5e199161e5b4cb750642a483aa6831634ab0a5ec
iam_chandra3 <chandrasekhar_v@srmap.edu.in>
2022-10-24 20:24:31 +0530

    Added sorting and searching algos in python

---
+++ Algorithms/sorting/quicksort.py
@@ -0,0 +1,16 @@
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+
+def main():
+    array = [10, 7, 8, 9, 1, 5]
+    print(quicksort(array))
+
+
+main()
